910dfd757c61c95562621de7578a1b4557020d27
Berkanoid <dynomike@mail.ru>
2026-03-14 02:36:32 +0300

    Update Program.cs

--- tasks/task19/Program.cs
+++ tasks/task19/Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 namespace Task19
 {
+    public enum NodeColor { Red, Black }
     public class TreeMapEntry<K, V>
     {
         public K Key;
@@ -12,10 +12,12 @@ namespace Task19
         public TreeMapEntry<K, V> Left;
         public TreeMapEntry<K, V> Right;
         public TreeMapEntry<K, V> Parent;
-        public TreeMapEntry(K key, V value) 
-        { 
-          Key = key; 
-          Value = value; 
+        public NodeColor Color;
+        public TreeMapEntry(K key, V value)
+        {
+            Key = key;
+            Value = value;
+            Color = NodeColor.Red;
         }
     }
     public class MyTreeMap<K, V> where K : IComparable<K>
@@ -43,23 +45,113 @@ namespace Task19
         }
         public void Put(K key, V value)
         {
-            if (root == null) { root = new TreeMapEntry<K, V>(key, value); size = 1; return; }
-            var cur = root;
-            TreeMapEntry<K, V> parent = null;
-            while (cur != null)
+            if (key == null) throw new ArgumentNullException(nameof(key));
+            if (root == null)
             {
-                parent = cur;
-                int cmp = comparator.Compare(key, cur.Key);
-                if (cmp < 0) cur = cur.Left;
-                else if (cmp > 0) cur = cur.Right;
-                else { cur.Value = value; return; }
+                root = new TreeMapEntry<K, V>(key, value);
+                root.Color = NodeColor.Black;
+                size = 1;
+                return;
             }
-            var newNode = new TreeMapEntry<K, V>(key, value);
-            newNode.Parent = parent;
-            if (comparator.Compare(key, parent.Key) < 0) parent.Left = newNode;
-            else parent.Right = newNode;
+            TreeMapEntry<K, V> t = root;
+            TreeMapEntry<K, V> parent = null;
+            int cmp = 0;
+            while (t != null)
+            {
+                parent = t;
+                cmp = comparator.Compare(key, t.Key);
+                if (cmp < 0) t = t.Left;
+                else if (cmp > 0) t = t.Right;
+                else { t.Value = value; return; }
+            }
+            var e = new TreeMapEntry<K, V>(key, value) { Parent = parent };
+            if (cmp < 0) parent.Left = e; else parent.Right = e;
             size++;
+            FixAfterInsertion(e);
         }
+        private void RotateLeft(TreeMapEntry<K, V> p)
+        {
+            if (p == null) return;
+            var r = p.Right;
+            p.Right = r.Left;
+            if (r.Left != null) r.Left.Parent = p;
+            r.Parent = p.Parent;
+            if (p.Parent == null) root = r;
+            else if (p.Parent.Left == p) p.Parent.Left = r;
+            else p.Parent.Right = r;
+            r.Left = p;
+            p.Parent = r;
+        }
+        private void RotateRight(TreeMapEntry<K, V> p)
+        {
+            if (p == null) return;
+            var l = p.Left;
+            p.Left = l.Right;
+            if (l.Right != null) l.Right.Parent = p;
+            l.Parent = p.Parent;
+            if (p.Parent == null) root = l;
+            else if (p.Parent.Right == p) p.Parent.Right = l;
+            else p.Parent.Left = l;
+            l.Right = p;
+            p.Parent = l;
+        }
+        private void FixAfterInsertion(TreeMapEntry<K, V> x)
+        {
+            x.Color = NodeColor.Red;
+            while (x != null && x != root && x.Parent.Color == NodeColor.Red)
+            {
+                if (ParentOf(x) == LeftOf(ParentOf(ParentOf(x))))
+                {
+                    var y = RightOf(ParentOf(ParentOf(x)));
+                    if (ColorOf(y) == NodeColor.Red)
+                    {
+                        SetColor(ParentOf(x), NodeColor.Black);
+                        SetColor(y, NodeColor.Black);
+                        SetColor(ParentOf(ParentOf(x)), NodeColor.Red);
+                        x = ParentOf(ParentOf(x));
+                    }
+                    else
+                    {
+                        if (x == RightOf(ParentOf(x)))
+                        {
+                            x = ParentOf(x);
+                            RotateLeft(x);
+                        }
+                        SetColor(ParentOf(x), NodeColor.Black);
+                        SetColor(ParentOf(ParentOf(x)), NodeColor.Red);
+                        RotateRight(ParentOf(ParentOf(x)));
+                    }
+                }
+                else
+                {
+                    var y = LeftOf(ParentOf(ParentOf(x)));
+                    if (ColorOf(y) == NodeColor.Red)
+                    {
+                        SetColor(ParentOf(x), NodeColor.Black);
+                        SetColor(y, NodeColor.Black);
+                        SetColor(ParentOf(ParentOf(x)), NodeColor.Red);
+                        x = ParentOf(ParentOf(x));
+                    }
+                    else
+                    {
+                        if (x == LeftOf(ParentOf(x)))
+                        {
+                            x = ParentOf(x);
+                            RotateRight(x);
+                        }
+                        SetColor(ParentOf(x), NodeColor.Black);
+                        SetColor(ParentOf(ParentOf(x)), NodeColor.Red);
+                        RotateLeft(ParentOf(ParentOf(x)));
+                    }
+                }
+            }
+            root.Color = NodeColor.Black;
+        }
+        private static TreeMapEntry<K, V> ParentOf(TreeMapEntry<K, V> n) => n != null ? n.Parent : null;
+        private static TreeMapEntry<K, V> LeftOf(TreeMapEntry<K, V> n) => n != null ? n.Left : null;
+        private static TreeMapEntry<K, V> RightOf(TreeMapEntry<K, V> n) => n != null ? n.Right : null;
+        private static NodeColor ColorOf(TreeMapEntry<K, V> n) => n != null ? n.Color : NodeColor.Black;
+        private static void SetColor(TreeMapEntry<K, V> n, NodeColor c) { if (n != null) n.Color = c; }
         public bool Remove(K key)
         {
             var node = FindNode(key);
@@ -68,44 +160,131 @@ namespace Task19
             size--;
             return true;
         }
-        private void DeleteNode(TreeMapEntry<K, V> node)
+        private void DeleteNode(TreeMapEntry<K, V> p)
         {
-            if (node.Left == null && node.Right == null) ReplaceNode(node, null);
-            else if (node.Left == null) ReplaceNode(node, node.Right);
-            else if (node.Right == null) ReplaceNode(node, node.Left);
+            if (p.Left != null && p.Right != null)
+            {
+                var s = GetMin(p.Right);
+                p.Key = s.Key;
+                p.Value = s.Value;
+                p = s;
+            }
+            var replacement = (p.Left != null) ? p.Left : p.Right;
+            if (replacement != null)
+            {
+                replacement.Parent = p.Parent;
+                if (p.Parent == null) root = replacement;
+                else if (p == p.Parent.Left) p.Parent.Left = replacement;
+                else p.Parent.Right = replacement;
+                p.Left = p.Right = p.Parent = null;
+                if (p.Color == NodeColor.Black)
+                    FixAfterDeletion(replacement);
+            }
+            else if (p.Parent == null)
+            {
+                root = null;
+            }
             else
             {
-                var succ = GetMin(node.Right);
-                node.Key = succ.Key;
-                node.Value = succ.Value;
-                DeleteNode(succ);
+                if (p.Color == NodeColor.Black)
+                    FixAfterDeletion(p);
+                if (p.Parent != null)
+                {
+                    if (p == p.Parent.Left) p.Parent.Left = null;
+                    else if (p == p.Parent.Right) p.Parent.Right = null;
+                    p.Parent = null;
+                }
             }
         }
-        private void ReplaceNode(TreeMapEntry<K, V> node, TreeMapEntry<K, V> child)
+        private void FixAfterDeletion(TreeMapEntry<K, V> x)
         {
-            if (node.Parent == null) root = child;
-            else if (node.Parent.Left == node) node.Parent.Left = child;
-            else node.Parent.Right = child;
-            if (child != null) child.Parent = node.Parent;
+            while (x != root && ColorOf(x) == NodeColor.Black)
+            {
+                if (x == LeftOf(ParentOf(x)))
+                {
+                    var sib = RightOf(ParentOf(x));
+                    if (ColorOf(sib) == NodeColor.Red)
+                    {
+                        SetColor(sib, NodeColor.Black);
+                        SetColor(ParentOf(x), NodeColor.Red);
+                        RotateLeft(ParentOf(x));
+                        sib = RightOf(ParentOf(x));
+                    }
+                    if (ColorOf(LeftOf(sib)) == NodeColor.Black && ColorOf(RightOf(sib)) == NodeColor.Black)
+                    {
+                        SetColor(sib, NodeColor.Red);
+                        x = ParentOf(x);
+                    }
+                    else
+                    {
+                        if (ColorOf(RightOf(sib)) == NodeColor.Black)
+                        {
+                            SetColor(LeftOf(sib), NodeColor.Black);
+                            SetColor(sib, NodeColor.Red);
+                            RotateRight(sib);
+                            sib = RightOf(ParentOf(x));
+                        }
+                        SetColor(sib, ColorOf(ParentOf(x)));
+                        SetColor(ParentOf(x), NodeColor.Black);
+                        SetColor(RightOf(sib), NodeColor.Black);
+                        RotateLeft(ParentOf(x));
+                        x = root;
+                    }
+                }
+                else
+                {
+                    var sib = LeftOf(ParentOf(x));
+                    if (ColorOf(sib) == NodeColor.Red)
+                    {
+                        SetColor(sib, NodeColor.Black);
+                        SetColor(ParentOf(x), NodeColor.Red);
+                        RotateRight(ParentOf(x));
+                        sib = LeftOf(ParentOf(x));
+                    }
+                    if (ColorOf(RightOf(sib)) == NodeColor.Black && ColorOf(LeftOf(sib)) == NodeColor.Black)
+                    {
+                        SetColor(sib, NodeColor.Red);
+                        x = ParentOf(x);
+                    }
+                    else
+                    {
+                        if (ColorOf(LeftOf(sib)) == NodeColor.Black)
+                        {
+                            SetColor(RightOf(sib), NodeColor.Black);
+                            SetColor(sib, NodeColor.Red);
+                            RotateLeft(sib);
+                            sib = LeftOf(ParentOf(x));
+                        }
+                        SetColor(sib, ColorOf(ParentOf(x)));
+                        SetColor(ParentOf(x), NodeColor.Black);
+                        SetColor(LeftOf(sib), NodeColor.Black);
+                        RotateRight(ParentOf(x));
+                        x = root;
+                    }
+                }
+            }
+            if (x != null) x.Color = NodeColor.Black;
         }
         private TreeMapEntry<K, V> GetMin(TreeMapEntry<K, V> node)
         {
+            if (node == null) return null;
             while (node.Left != null) node = node.Left;
             return node;
         }
         private TreeMapEntry<K, V> GetMax(TreeMapEntry<K, V> node)
         {
+            if (node == null) return null;
             while (node.Right != null) node = node.Right;
             return node;
         }
         public K FirstKey()
         {
-            if (root == null) throw new InvalidOperationException();
+            if (root == null) throw new InvalidOperationException("Map is empty");
             return GetMin(root).Key;
         }
         public K LastKey()
         {
-            if (root == null) throw new InvalidOperationException();
+            if (root == null) throw new InvalidOperationException("Map is empty");
             return GetMax(root).Key;
         }
         public K LowerKey(K key)
@@ -240,15 +419,19 @@ namespace Task19
         private void AddTail(TreeMapEntry<K, V> node, K start, MyTreeMap<K, V> map)
         {
             if (node == null) return;
-            AddTail(node.Right, start, map);
-            if (comparator.Compare(node.Key, start) > 0)
+            AddTail(node.Left, start, map);
+            if (comparator.Compare(node.Key, start) >= 0)
             {
                 map.Put(node.Key, node.Value);
-                AddTail(node.Left, start, map);
+                AddTail(node.Right, start, map);
+            }
+            else
+            {
+                AddTail(node.Right, start, map);
             }
         }
     }
-    public class MyTreeSet<E> where E : IComparable<E>
+    public class MyTreeSet<E> : IEnumerable<E> where E : IComparable<E>
     {
         private readonly MyTreeMap<E, object> map;
         private readonly IComparer<E> comparator;
@@ -256,38 +439,53 @@ namespace Task19
         public MyTreeSet() : this(Comparer<E>.Default) { }
         public MyTreeSet(IComparer<E> comparator)
         {
-            this.comparator = comparator;
-            map = new MyTreeMap<E, object>(comparator);
+            this.comparator = comparator ?? Comparer<E>.Default;
+            map = new MyTreeMap<E, object>(this.comparator);
         }
         public MyTreeSet(MyTreeMap<E, object> map)
         {
             this.map = map ?? throw new ArgumentNullException(nameof(map));
             this.comparator = map.Comparator;
         }
-        public MyTreeSet(E[] a) : this() { AddAll(a); }
-        public MyTreeSet(SortedSet<E> s) : this() { foreach (var e in s) Add(e); }
-        public void Add(E e) => map.Put(e, dummy);
-        public void AddAll(E[] a) { foreach (var e in a) Add(e); }
+        public MyTreeSet(E[] a) : this()
+        {
+            if (a != null) AddAll(a);
+        }
+        public MyTreeSet(SortedSet<E> s) : this()
+        {
+            if (s != null)
+                foreach (var e in s) Add(e);
+        }
+        public void Add(E e)
+        {
+            if (e == null) throw new ArgumentNullException(nameof(e));
+            map.Put(e, dummy);
+        }
+        public void AddAll(E[] a) { if (a == null) return; foreach (var e in a) if (e != null) Add(e); }
         public void Clear() => map.Clear();
         public bool Contains(object o)
         {
+            if (o == null) return false;
             if (o is E e) return map.ContainsKey(e);
             return false;
         }
         public bool ContainsAll(E[] a)
         {
+            if (a == null) return true;
             foreach (var e in a) if (!Contains(e)) return false;
             return true;
         }
         public bool IsEmpty() => map.IsEmpty();
         public bool Remove(object o)
         {
+            if (o == null) return false;
             if (o is E e) return map.Remove(e);
             return false;
         }
-        public void RemoveAll(E[] a) { foreach (var e in a) Remove(e); }
+        public void RemoveAll(E[] a) { if (a == null) return; foreach (var e in a) Remove(e); }
         public void RetainAll(E[] a)
         {
+            if (a == null) { Clear(); return; }
             var keep = new HashSet<E>(a);
             var toRemove = new List<E>();
             foreach (var e in map.KeySet())
@@ -321,26 +519,31 @@ namespace Task19
             new MyTreeSet<E>(map.TailMap(fromElement));
         public E Ceiling(E obj)
         {
+            if (obj == null) return default;
             var entry = map.CeilingEntry(obj);
             return entry != null ? entry.Key : default;
         }
         public E Floor(E obj)
         {
+            if (obj == null) return default;
             var entry = map.FloorEntry(obj);
             return entry != null ? entry.Key : default;
         }
         public E Higher(E obj)
         {
+            if (obj == null) return default;
             var entry = map.HigherEntry(obj);
             return entry != null ? entry.Key : default;
         }
         public E Lower(E obj)
         {
+            if (obj == null) return default;
             var entry = map.LowerEntry(obj);
             return entry != null ? entry.Key : default;
         }
         public MyTreeSet<E> HeadSet(E upperBound, bool inclusive)
         {
+            if (upperBound == null) throw new ArgumentNullException(nameof(upperBound));
             if (inclusive)
             {
                 var result = new MyTreeSet<E>(comparator);
@@ -352,6 +555,8 @@ namespace Task19
         }
         public MyTreeSet<E> SubSet(E lowerBound, bool lowIncl, E upperBound, bool highIncl)
         {
+            if (lowerBound == null) throw new ArgumentNullException(nameof(lowerBound));
+            if (upperBound == null) throw new ArgumentNullException(nameof(upperBound));
             var result = new MyTreeSet<E>(comparator);
             foreach (var e in map.KeySet())
             {
@@ -365,6 +570,7 @@ namespace Task19
         }
         public MyTreeSet<E> TailSet(E fromElement, bool inclusive)
         {
+            if (fromElement == null) throw new ArgumentNullException(nameof(fromElement));
             if (inclusive)
             {
                 var result = new MyTreeSet<E>(comparator);
@@ -397,22 +603,29 @@ namespace Task19
             foreach (var e in map.KeySet()) revSet.Add(e);
             return revSet;
         }
+        public IEnumerator<E> GetEnumerator()
+        {
+            foreach (var e in map.KeySet()) yield return e;
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("=== Тестирование MyTreeSet ===");
+            Console.WriteLine("Тестирование MyTreeSet (Red-Black)");
             var set = new MyTreeSet<string>();
             set.Add("banana");
             set.Add("apple");
             set.Add("cherry");
             set.Add("date");
+            set.Add("fig");
+            set.Add("eggfruit");
             Console.WriteLine("Размер: " + set.Size());
             Console.WriteLine("Первый: " + set.First());
             Console.WriteLine("Последний: " + set.Last());
-            Console.WriteLine("Содержит 'cherry'? " + set.Contains("cherry"));
-            Console.WriteLine("Содержит 'fig'? " + set.Contains("fig"));
+            Console.WriteLine("Содержит cherry? " + set.Contains("cherry"));
+            Console.WriteLine("Содержит grape? " + set.Contains("grape"));
             Console.WriteLine("Все элементы:");
             foreach (var e in set.ToArray()) Console.WriteLine("  " + e);
             Console.WriteLine("Ceiling(c): " + set.Ceiling("c"));
@@ -433,6 +646,8 @@ namespace Task19
             while (desc.MoveNext()) Console.WriteLine("  " + desc.Current);
             var descSet = set.DescendingSet();
             Console.WriteLine("Обратное множество: " + string.Join(", ", descSet.ToArray()));
+            Console.WriteLine("Foreach (прямой):");
+            foreach (var item in set) Console.WriteLine("  " + item);
         }
     }
 }
